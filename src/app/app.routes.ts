47cbb4ca1c12a690a1a1db5a975f3d7569d17aec
Elven Sajulga <sajulga.johnelven@gmail.com>
2026-02-07 09:01:24 +0800

    fixed_missing_log-in_page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,19 +2,19 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './auth/login/login';
 import { Dashboard } from './home/dashboard/dashboard';
 import { Announcements } from './pages/announcements/announcements';
-import { assertNotInReactiveContext } from '@angular/core';
 import { Assignments } from './pages/assignments/assignments';
 import { Attendance } from './pages/attendance/attendance';
 import { Grades } from './pages/grades/grades';
 import { Settings } from './pages/settings/settings';
 
 export const routes: Routes = [
+  { path: '', component: LoginComponent },
   { path: 'login', component: LoginComponent },
-
   { path: 'dashboard', component: Dashboard },
   { path: 'announcements', component: Announcements },
-  { path: 'assignment', component: Assignments},
-  { path: 'attendance', component: Attendance},
-  { path: 'grades', component: Grades},
-  { path: 'settings', component: Settings},
+  { path: 'assignment', component: Assignments },
+  { path: 'attendance', component: Attendance },
+  { path: 'grades', component: Grades },
+  { path: 'settings', component: Settings },
 ];
+
